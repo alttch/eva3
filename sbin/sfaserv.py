f6999631303b9fd89508254f88b1303f4f42a0cf
root <root@lab-d-s2.lab.office.altertech.net>
2018-06-17 14:07:41 +0200

    new fw func

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -42,7 +42,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2018061702
+product_build = 2018061703
 
 product_code = 'sfa'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2018061702
+product_build = 2018061703
 
 product_code = 'uc'
 
