0ba0fdbdfbf224f8a43a06a34ac4244f96a5a4c4
divisor <div@altertech.com>
2018-06-28 18:43:19 +0200

    id check fix

--- easy-setup.sh
+++ easy-setup.sh
@@ -158,7 +158,7 @@ function askUser {
         if [ "x$u" == "x" ]; then
             u=$2
         fi
-        id $u > dev/null 2>&1
+        id $u > /dev/null 2>&1
         if [ $? -eq 0 ]; then
             USER=$u
             return
