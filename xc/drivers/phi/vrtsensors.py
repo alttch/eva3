f4b768e7e033c228637f6873032a0041d0588574
divisor <div@altertech.com>
2018-07-04 02:17:55 +0200

    vrt phis

---
+++ xc/drivers/phi/vrtsensors.py
@@ -0,0 +1,68 @@
+__author__ = "Altertech Group, https://www.altertech.com/"
+__copyright__ = "Copyright (C) 2012-2018 Altertech Group"
+__license__ = "https://www.eva-ics.com/license"
+__version__ = "1.0.0"
+__description__ = "Emulates virtual sensors"
+__api__ = 1
+
+__id__ = 'vrtsensors'
+__equipment__ = 'virtual sensors'
+
+from eva.uc.drivers.phi.generic_phi import PHI as GenericPHI
+from eva.uc.driverapi import handle_phi_event
+from eva.uc.driverapi import log_traceback
+from eva.uc.driverapi import critical
+
+import logging
+
+
+class PHI(GenericPHI):
+
+    def __init__(self, phi_cfg=None):
+        super().__init__(phi_cfg=phi_cfg)
+        d = self.phi_cfg.get('default_value')
+        if d is None: d = None
+        else:
+            try:
+                d = int(d)
+            except:
+                d = None
+        self.data = {}
+        for i in range(1000, 1010):
+            self.data[str(i)] = d
+        self.phi_mod_id = __id__
+        self.__author = __author__
+        self.__license = __license__
+        self.__description = __description__
+        self.__version = __version__
+        self.__api_version = __api__
+        self.__equipment = __equipment__
+
+    def get(self, port=None, timeout=None):
+        try:
+            return self.data.get(str(port))
+        except:
+            return None
+
+    def serialize(self, full=False, config=False):
+        d = super().serialize(full=full, config=config)
+        return d
+
+    def test(self, cmd=None):
+        if cmd == 'get':
+            return self.data
+        if cmd == 'critical':
+            critical()
+            return { 'result': 'OK' }
+        try:
+            port, val = cmd.split('=')
+            val = float(val)
+            self.data[port] = val
+            logging.debug(
+                '%s test completed, set port %s=%s' % (self.phi_id, port, val))
+            if self.phi_cfg.get('event_on_test_set'):
+                handle_phi_event(self, port, self.data)
+            return self.data
+        except:
+            log_traceback()
+            return None
