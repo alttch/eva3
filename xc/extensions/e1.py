5d6aef87d68c3dd5720795fae94aeba5b1561968
divisor <div@altertech.com>
2018-07-05 23:11:24 +0200

    lm extensions draft

---
+++ xc/extensions/e1.py
@@ -0,0 +1,50 @@
+__author__ = "Altertech Group, https://www.altertech.com/"
+__copyright__ = "Copyright (C) 2012-2018 Altertech Group"
+__license__ = "https://www.eva-ics.com/license"
+__version__ = "1.0.0"
+__description__ = "Posts JSON data to the URL"
+__api__ = 1
+
+__id__ = 'e1'
+
+__config_help__ = {
+    'url': 'url to push JSON data to'
+    }
+
+__functions__ = {
+        'push': 'push data to remote URL'
+        }
+
+from eva.lm.extensions.generic import LMExt as GenericExt
+
+from eva.lm.extapi import get_timeout
+from eva.lm.extapi import log_traceback
+
+import requests
+import jsonpickle
+
+class LMExt(GenericExt):
+
+    def __init__(self, cfg=None):
+        super().__init__(cfg)
+        self.__author = __author__
+        self.__license = __license__
+        self.__description = __description__
+        self.__version = __version__
+        self.__api_version = __api__
+        self.__lpi_mod_id = __id__
+        self.__logic = __logic__
+        self.__features = __features__
+        self.__config_help = __config_help__
+        self.url = self.cfg.get('url')
+        if not self.url: self.ready = False
+
+    def push(self, data, timeout=None):
+        _timeout = timeout if timeout else get_timeout()
+        r = requests.post(self.url, json=data, timeout=_timeout)
+        if r.status_code != 200:
+            return None
+        try:
+            return jsonpickle.decode(r.text)
+        except:
+            return None
---
+++ xc/extensions/generic.py
@@ -0,0 +1,86 @@
+__author__ = "Altertech Group, https://www.altertech.com/"
+__copyright__ = "Copyright (C) 2012-2018 Altertech Group"
+__license__ = "https://www.eva-ics.com/license"
+__version__ = "1.0.0"
+__description__ = "Generic macro extension, don't use"
+__api__ = 1
+
+__id__ = 'generic'
+
+__functions__ = {}
+
+__config_help__ = {}
+
+import logging
+
+from eva.lm.extapi import critical
+
+class LMExt(object):
+    """
+    Override everything. super() constructor may be useful to keep unparsed
+    config
+    """
+
+    def __init__(self, cfg=None):
+        if cfg:
+            self.cfg = cfg
+        else:
+            self.cfg = {}
+        self.ext_mod_id = __id__
+        self.__author = __author__
+        self.__license = __license__
+        self.__description = __description__
+        self.__version = __version__
+        self.__api_version = __api__
+        self.__functions = __functions__
+        self.__config_help = __config_help__
+        self.ready = True
+        self.ext_id = None  # set by extapi on load
+
+    def start(self):
+        return True
+
+    def stop(self):
+        return True
+
+    def serialize(self, full=False, config=False):
+        d = {}
+        if full:
+            d['author'] = self.__author
+            d['license'] = self.__license
+            d['description'] = self.__description
+            d['version'] = self.__version
+            d['api'] = self.__api_version
+            d['functions'] = self.__functions
+        if config:
+            d['cfg'] = self.cfg
+        d['mod'] = self.ext_mod_id
+        d['id'] = self.ext_id
+        if not self.ext_id:
+            d['cfg'] = self.__config_help
+        return d
+
+    def test(self, cmd=None):
+        return None
+
+    def log_debug(self, msg):
+        logging.debug('Extension %s: %s' % (self.ext_id, msg))
+
+    def log_info(self, msg):
+        logging.info('Extension %s: %s' % (self.ext_id, msg))
+
+    def log_warning(self, msg):
+        logging.warning('Extension %s: %s' % (self.ext_id, msg))
+
+    def log_error(self, msg):
+        logging.error('Extension %s: %s' % (self.ext_id, msg))
+
+    def log_error(self, msg):
+        logging.error('Extension %s: %s' % (self.ext_id, msg))
+
+    def log_critical(self, msg):
+        self.critical(msg)
+
+    def critical(self, msg):
+        logging.critical('Extension %s: %s' % (self.ext_id, msg))
+        critical()
