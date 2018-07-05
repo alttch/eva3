03f4b6451b66647c9854e0e441530fade8c93ee6
divisor <div@altertech.com>
2018-07-05 13:22:11 +0200

    fix driver path

--- install-uc.sh
+++ install-uc.sh
@@ -46,7 +46,7 @@ ln -sf ../runtime/xc/uc
 
 cd ..
 
-ln -sf ../../xc/drivers lib/eva/uc/drivers
+ln -sf ../../../xc/drivers lib/eva/uc/drivers
 
 echo "Checking mods"
 ./sbin/check_mods install || exit 1
