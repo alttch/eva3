72c8f716c495c7947b234e0da536523cd1eac29f
divisor <div@altertech.com>
2018-07-08 17:54:00 +0200

    universal

--- xc/drivers/phi/dae_pbro5ip.py
+++ xc/drivers/phi/dae_pbro5ip.py
@@ -8,7 +8,7 @@ __id__ = 'dae_pbro5ip'
 __equipment__ = 'DAE-PB-RO5-DAEnetIP4'
 __api__ = 1
 __required__ = ['port_get', 'port_set', 'status', 'action']
-__features__ = ['port_get', 'port_set']
+__features__ = ['port_get', 'port_set', 'universal']
 __config_help__ = [{
     'name': 'host',
     'help': 'relay host/ip[:port]',
@@ -41,11 +41,20 @@ __help__ = """
 PHI for Denkovi relay DAE-PB-RO5-DAEnetIP4, uses SNMP API to control/monitor
 the equipment. SNMP on relay should be enabled and configured to allow packets
 from UC.
+
+This is unversal PHI which means one PHI can control either one or multiple
+relays of the same type if relay config (host) is provided in unit driver
+configuration.
+
+host and communities should be specified either in driver primary configuration
+or in each unit configuration which uses the driver with this PHI.
 """
 
 from eva.uc.drivers.phi.generic_phi import PHI as GenericPHI
 from eva.uc.driverapi import log_traceback
 from eva.uc.driverapi import get_timeout
+
+from eva.tools import parse_host_port
 
 import eva.uc.drivers.tools.snmp as snmp
 
@@ -81,18 +90,10 @@ class PHI(GenericPHI):
             self.snmp_tries = int(self.phi_get('retries')) + 1
         except:
             self.snmp_tries = 1
-        host = self.phi_cfg.get('host')
         self.port_shift = 7
         self.port_max = 5
-        if host:
-            try:
-                self.snmp_host, port = host.split(':')
-                self.snmp_port = int(port)
-            except:
-                self.snmp_host = host
-                self.snmp_port = 161
-        else:
-            self.ready = False
+        self.snmp_host, self.snmp_port = parse_host_port(
+            self.phi_cfg.get('host'), 161)
         self.oid_name = '.1.3.6.1.4.1.42505.1.1.1.0'
         self.oid_version = '.1.3.6.1.4.1.42505.1.1.2.0'
         self.oid_work = '.1.3.6.1.4.1.42505.1.2.3.1.11'
@@ -102,15 +103,35 @@ class PHI(GenericPHI):
             port = int(port)
         except:
             return None
+        if cfg:
+            host, snmp_port = parse_host_port(cfg.get('host'), 161)
+            community = cfg.get('community')
+            if not community: community = cfg.get('read_community')
+            tries = cfg.get('retries')
+            try:
+                tries = int(tries)
+            except:
+                tries = None
+        else:
+            host = None
+            community = None
+            tries = None
+        if not host:
+            host = self.snmp_host
+            snmp_port = self.snmp_port
+        if not community:
+            community = self.snmp_read_community
+        if tries is None: tries = self.snmp_tries
+        if not host or not community: return None
         if port < 1 or port > self.port_max: return None
-        _timeout = (timeout - 1) / self.snmp_tries
+        _timeout = (timeout - 1) / tries
         return snmp.get(
             '%s.%u' % (self.oid_work, port + self.port_shift),
-            self.snmp_host,
-            self.snmp_port,
-            self.snmp_read_community,
+            host,
+            snmp_port,
+            community,
             _timeout,
-            self.snmp_tries - 1,
+            tries - 1,
             rf=int)
 
     def set(self, port=None, data=None, cfg=None, timeout=0):
@@ -119,14 +140,34 @@ class PHI(GenericPHI):
             val = int(data)
         except:
             return None
+        if cfg:
+            host, snmp_port = parse_host_port(cfg.get('host'), 161)
+            community = cfg.get('community')
+            if not community: community = cfg.get('write_community')
+            tries = cfg.get('retries')
+            try:
+                tries = int(tries)
+            except:
+                tries = None
+        else:
+            host = None
+            community = None
+            tries = None
+        if not host:
+            host = self.snmp_host
+            snmp_port = self.snmp_port
+        if not community:
+            community = self.snmp_write_community
+        if tries is None: tries = self.snmp_tries
+        if not host or not community: return None
         if port < 1 or port > self.port_max or val < 0 or val > 1: return None
         _timeout = (timeout - 1) / self.snmp_tries
         return snmp.set('%s.%u' % (self.oid_work, port + self.port_shift),
-                        rfc1902.Integer(val), self.snmp_host, self.snmp_port,
-                        self.snmp_write_community, _timeout,
-                        self.snmp_tries - 1)
+                        rfc1902.Integer(val), host, snmp_port, community,
+                        _timeout, tries - 1)
 
     def test(self, cmd=None):
+        if cmd == 'self' and self.snmp_host is None: return 'OK'
         if cmd == 'info' or cmd == 'self':
             name = snmp.get(
                 self.oid_name,
