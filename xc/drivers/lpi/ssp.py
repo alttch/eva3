e4be91e3b45cf9dca5c7c1c8fe5105f655c52fda
divisor <div@altertech.com>
2018-07-14 16:18:07 +0200

    ssp

---
+++ xc/drivers/lpi/ssp.py
@@ -0,0 +1,67 @@
+__author__ = "Altertech Group, https://www.altertech.com/"
+__copyright__ = "Copyright (C) 2012-2018 Altertech Group"
+__license__ = "https://www.eva-ics.com/license"
+__version__ = "1.0.0"
+__description__ = "Single port sensor LPI"
+__api__ = 1
+
+__id__ = 'ssp'
+__logic__ = 'single port'
+
+__features__ = ['value', 'port_get', 'events', 'ssp']
+
+__config_help__ = []
+
+__action_help__ = []
+
+__state_help__ = []
+
+__state_help__ += __config_help__
+
+__help__ = """
+LPI to work with sensor PHIs which provide the state for single sensor only
+(port=1), doesn't process sensor value in any way, returning it to controller
+as-is. """
+
+from time import time
+
+from eva.uc.drivers.lpi.generic_lpi import LPI as GenericLPI
+
+from eva.tools import val_to_boolean
+
+
+class LPI(GenericLPI):
+
+    def __init__(self, lpi_cfg=None, phi_id=None, info_only=False):
+        super().__init__(lpi_cfg, phi_id, info_only)
+        self.lpi_mod_id = __id__
+        self.__author = __author__
+        self.__license = __license__
+        self.__description = __description__
+        self.__version = __version__
+        self.__api_version = __api__
+        self.__lpi_mod_id = __id__
+        self.__logic = __logic__
+        self.__features = __features__
+        self.__config_help = __config_help__
+        self.__action_help = __action_help__
+        self.__state_help = __state_help__
+        self.__help = __help__
+        # if info_only: return
+
+    def do_state(self, _uuid, cfg, timeout, tki, state_in):
+        time_start = time()
+        _state_in = state_in
+        if _state_in: evh = True
+        else: evh = False
+        phi_cfg = self.prepare_phi_cfg(cfg)
+        if _state_in:
+            value = _state_in.get('1')
+        else:
+            value = self.phi.get('1', phi_cfg, timeout + time_start - time())
+        if value is None and evh:
+            return self.state_result_skip(_uuid)
+        if value is None or value is False:
+            return self.state_result_error(_uuid)
+        self.set_result(_uuid, (1, str(value)))
+        return
