38b49f3ae104ca7649f9281484685b854fdea7f1
divisor <div@altertech.com>
2018-07-25 00:51:19 +0200

    subscribe/unsubscribe

--- bin/notifymanager.py
+++ bin/notifymanager.py
@@ -32,27 +32,134 @@ class NotifierCLI(GenericCLI):
                 'json:http(s)://[key]@uri or ' + \
                 'mqtt:[username:password]@host:[port] or ' + \
                 'db:dbfile[:keeptime]', metavar='PROPS')
-        ap_create.add_argument('-s', '--space', help='Notification space', metavar='SPACE', dest='s')
-        ap_create.add_argument('-t', '--timeout', help='Notifier timeout', metavar='SEC', dest='t', type=float)
-        ap_create.add_argument('-y', '--enable', help='Enable notifier after creation', dest='y', action='store_true')
+        ap_create.add_argument(
+            '-s',
+            '--space',
+            help='Notification space',
+            metavar='SPACE',
+            dest='s')
+        ap_create.add_argument(
+            '-t',
+            '--timeout',
+            help='Notifier timeout',
+            metavar='SEC',
+            dest='t',
+            type=float)
+        ap_create.add_argument(
+            '-y',
+            '--enable',
+            help='Enable notifier after creation',
+            dest='y',
+            action='store_true')
         ap_enable = self.sp.add_parser('enable', help='Enable notifier')
-        ap_enable.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_enable.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_disable = self.sp.add_parser('disable', help='Disable notifier')
-        ap_disable.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_disable.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_config = self.sp.add_parser('config', help='Get notifier config')
-        ap_config.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_config.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_props = self.sp.add_parser('props', help='Get notifier properties')
-        ap_props.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_props.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_set_prop = self.sp.add_parser('set', help='Set notifier property')
-        ap_set_prop.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_set_prop.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_set_prop.add_argument('p', help='Config property', metavar='PROP')
         ap_set_prop.add_argument('v', help='Value', metavar='VAL', nargs='?')
         ap_test = self.sp.add_parser('test', help='Test notifier')
-        ap_test.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_test.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
         ap_destroy = self.sp.add_parser('destroy', help='Destroy notifier')
-        ap_destroy.add_argument('i', help='Notifier ID', metavar='ID')
+        ap_destroy.add_argument('i', help='Notifier ID', metavar='NOTIFIER_ID')
 
-    def create_notifier(self, params=None):
+        ap_unsubscribe = self.sp.add_parser(
+            'unsubscribe', help='Unsubscribe notifier')
+        ap_unsubscribe.add_argument(
+            'i', help='Notifier ID', metavar='NOTIFIER_ID')
+        ap_unsubscribe.add_argument(
+            's',
+            help='Notification subject (if empty - unsubscribe from all)',
+            metavar='SUBJECT',
+            nargs='?',
+            choices=['log', 'state', 'action'])
+
+        ap_subscribe = self.sp.add_parser(
+            'subscribe', help='Subscribe notifier')
+        sp_subscribe = ap_subscribe.add_subparsers(
+            dest='_func',
+            metavar='topic',
+            help='Subscription topics, new topic subscription replaces previous'
+        )
+
+        sp_subscribe_log = sp_subscribe.add_parser(
+            'log', help='Subscribe to log messages')
+        sp_subscribe_log.add_argument(
+            'i', help='Notifier ID', metavar='NOTIFIER_ID')
+        sp_subscribe_log.add_argument(
+            '-l',
+            '--level',
+            help='Log level (debug, info [default], warning, error or critical)',
+            dest='l',
+            metavar='LEVEL',
+            default=20)
+
+        sp_subscribe_state = sp_subscribe.add_parser(
+            'state', help='Subscribe to state updates')
+        sp_subscribe_state.add_argument(
+            'i', help='Notifier ID', metavar='NOTIFIER_ID')
+        sp_subscribe_state.add_argument(
+            '-v',
+            '--types',
+            help=
+            'Item types, comma separated (unit, sensor, lvar or # [default] for all)',
+            dest='v',
+            metavar='TYPE',
+            default='#')
+        sp_subscribe_state.add_argument(
+            '-i',
+            '--items',
+            help='Item IDs, comma separated or # for all',
+            dest='items',
+            metavar='ITEMS')
+        sp_subscribe_state.add_argument(
+            '-g',
+            '--groups',
+            help='Item groups, comma separated or # for all',
+            dest='g',
+            metavar='GROUPS')
+
+        sp_subscribe_action = sp_subscribe.add_parser(
+            'action', help='Subscribe to action status')
+        sp_subscribe_action.add_argument(
+            'i', help='Notifier ID', metavar='NOTIFIER_ID')
+        sp_subscribe_action.add_argument(
+            '-a',
+            '--action-status',
+            help=
+            'Action status, comma separated (created, pending, queued, refused, ' + \
+                    'dead, canceled, ignored, running, failed, terminated, ' + \
+                    'completed or # [default] for all)',
+            dest='a',
+            metavar='TYPE',
+            default='#')
+        sp_subscribe_action.add_argument(
+            '-v',
+            '--types',
+            help=
+            'Item types, comma separated (unit, sensor, lvar or # [default] for all)',
+            dest='v',
+            metavar='TYPE',
+            default='#')
+        sp_subscribe_action.add_argument(
+            '-i',
+            '--items',
+            help='Item IDs, comma separated or # for all',
+            dest='items',
+            metavar='ITEMS')
+        sp_subscribe_action.add_argument(
+            '-g',
+            '--groups',
+            help='Item groups, comma separated or # for all',
+            dest='g',
+            metavar='GROUPS')
+
+    def create_notifier(self, params):
         n = self.get_notifier(params['i'], pass_errors=True)
         if n:
             self.print_err('notifier %s already exists' % params['i'])
@@ -120,8 +227,10 @@ class NotifierCLI(GenericCLI):
         elif p[0] == 'db':
             dbfile = p[1]
             if len(p) > 2:
-                try: keep = int(p[2])
-                except: return self.local_func_result_failed
+                try:
+                    keep = int(p[2])
+                except:
+                    return self.local_func_result_failed
             else:
                 keep = None
             n = eva.notify.SQLiteNotifier(
@@ -134,7 +243,7 @@ class NotifierCLI(GenericCLI):
         eva.notify.save_notifier(notifier_id)
         return self.local_func_result_ok
 
-    def list_notifiers(self, params=None):
+    def list_notifiers(self, params):
         eva.notify.load(test=False, connect=False)
         result = []
         for i in sorted(
@@ -153,7 +262,8 @@ class NotifierCLI(GenericCLI):
                 n['params'] = 'db: %s' % i.db
             elif isinstance(i, eva.notify.MQTTNotifier):
                 if i.username is not None:
-                    n['params'] = '%s:<secret>@' % (i.username)
+                    n['params'] = '%s%s@' % (i.username, ':*'
+                                             if i.password else '')
                 n['params'] += '%s:%u' % (i.host, i.port)
             result.append(n)
         return 0, result
@@ -163,10 +273,11 @@ class NotifierCLI(GenericCLI):
             n = eva.notify.load_notifier(notifier_id, test=False, connect=False)
             return n
         except:
-            if not pass_errors: self.print_err('can not load notifier %s' % notifier_id)
+            if not pass_errors:
+                self.print_err('can not load notifier %s' % notifier_id)
             return None
 
-    def test_notifier(self, params=None):
+    def test_notifier(self, params):
         n = self.get_notifier(params['i'])
         if n and n.test():
             n.disconnect()
@@ -174,7 +285,7 @@ class NotifierCLI(GenericCLI):
         else:
             return self.local_func_result_failed
 
-    def destroy_notifier(self, params=None):
+    def destroy_notifier(self, params):
         n = self.get_notifier(params['i'])
         if n:
             notifier_fname = eva.core.format_cfg_fname('%s_notify.d/%s.json' % \
@@ -188,10 +299,10 @@ class NotifierCLI(GenericCLI):
         else:
             return self.local_func_result_failed
 
-    def enable_notifier(self, params=None):
+    def enable_notifier(self, params):
         return self.set_notifier_enable(params['i'], True)
 
-    def disable_notifier(self, params=None):
+    def disable_notifier(self, params):
         return self.set_notifier_enable(params['i'], False)
 
     def set_notifier_enable(self, notifier_id, e):
@@ -204,15 +315,15 @@ class NotifierCLI(GenericCLI):
         else:
             return self.local_func_result_failed
 
-    def get_notifier_config(self, params=None):
+    def get_notifier_config(self, params):
         n = self.get_notifier(params['i'])
         return 0, n.serialize() if n else None
 
-    def list_notifier_props(self, params=None):
+    def list_notifier_props(self, params):
         n = self.get_notifier(params['i'])
         return 0, n.serialize(props=True) if n else None
 
-    def set_notifier_prop(self, params=None):
+    def set_notifier_prop(self, params):
         n = self.get_notifier(params['i'])
         prop = params.get('p')
         value = params.get('v')
@@ -236,13 +347,70 @@ class NotifierCLI(GenericCLI):
         eva.notify.save_notifier(params['i'])
         return self.local_func_result_ok
 
+    def subscribe_notifier_log(self, params):
+        n = self.get_notifier(params['i'])
+        if not n or not n.subscribe(subject='log', log_level=params.get('l')):
+            return self.local_func_result_failed
+        eva.notify.append_notifier(n)
+        eva.notify.save_notifier(params['i'])
+        for d in n.serialize()['events']:
+            if d['subject'] == 'log':
+                return 0, d
+        return self.local_func_result_failed
+
+    def subscribe_notifier_state(self, params):
+        n = self.get_notifier(params['i'])
+        if not n or not n.subscribe(
+                subject='state',
+                item_types=params['v'].split(',') if params.get('v') else None,
+                items=params['items'].split(',')
+                if params.get('items') else None,
+                groups=params['g'].split(',') if params.get('g') else None):
+            return self.local_func_result_failed
+        eva.notify.append_notifier(n)
+        eva.notify.save_notifier(params['i'])
+        for d in n.serialize()['events']:
+            if d['subject'] == 'state':
+                return 0, d
+        return local_func_result_failed
+
+    def subscribe_notifier_action(self, params):
+        n = self.get_notifier(params['i'])
+        if not n or not n.subscribe(
+                subject='action',
+                action_status=params['a'].split(',')
+                if params.get('a') else None,
+                item_types=params['v'].split(',') if params.get('v') else None,
+                items=params['items'].split(',')
+                if params.get('items') else None,
+                groups=params['g'].split(',') if params.get('g') else None):
+            return self.local_func_result_failed
+        eva.notify.append_notifier(n)
+        eva.notify.save_notifier(params['i'])
+        for d in n.serialize()['events']:
+            if d['subject'] == 'action':
+                return 0, d
+        return local_func_result_failed
+
+    def unsubscribe_notifier(self, params):
+        n = self.get_notifier(params['i'])
+        if not n or not n.unsubscribe(params['s'] if params.get('s') else '#'):
+            return local_func_result_failed
+        eva.notify.append_notifier(n)
+        eva.notify.save_notifier(params['i'])
+        return self.local_func_result_ok
+
 
 product = os.environ['EVA_PRODUCT']
 
 _me = 'EVA ICS Notification System Manager CLI for %s version %s' % (
     product.upper(), __version__)
 
-cli = NotifierCLI('%s_notifier' % product, _me, remote_api=False)
+cli = NotifierCLI(
+    '%s_notifier' % product,
+    _me,
+    remote_api=False,
+    prog='%s-notifier' % product)
 
 _api_functions = {
     'list': cli.list_notifiers,
@@ -253,7 +421,11 @@ _api_functions = {
     'disable': cli.disable_notifier,
     'config': cli.get_notifier_config,
     'props': cli.list_notifier_props,
-    'set': cli.set_notifier_prop
+    'set': cli.set_notifier_prop,
+    'unsubscribe': cli.unsubscribe_notifier,
+    'subscribe:log': cli.subscribe_notifier_log,
+    'subscribe:state': cli.subscribe_notifier_state,
+    'subscribe:action': cli.subscribe_notifier_action
 }
 
 _pd_cols = {'list_notifiers': ['id', 'type', 'enabled', 'params']}
@@ -275,6 +447,7 @@ _always_json = ['get_notifier_config']
 eva.core.set_product(product, '-1')
 cli.ap.prog = '%s-notifier' % product
 cli.always_json += _always_json
+cli.arg_sections += ['subscribe']
 cli.set_api_functions(_api_functions)
 cli.set_pd_cols(_pd_cols)
 cli.set_pd_idx(_pd_idx)
