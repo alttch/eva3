0417ddd7d50c881f8d9e6157e746b9cdaa9bcb30
divisor <div@altertech.com>
2018-09-01 01:36:52 +0200

    toc

--- doc/conf.py
+++ doc/conf.py
@@ -113,6 +113,9 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
+html_theme_options = {
+    'navigation_depth': 1,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
