7fb69f4700cb0caeaab6dfea9e9c6e2bc61e1bca
divisor <div@altertech.com>
2018-09-01 02:28:48 +0200

    sphinx 1.7.5

--- doc/conf.py
+++ doc/conf.py
@@ -107,18 +107,10 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # html_style = None
-html_theme_options = {
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-    }
-using_rtd_theme = True
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
