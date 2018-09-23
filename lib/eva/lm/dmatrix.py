511a58312354ac7d05bbcae8ba69f20a89391c8f
divisor <div@altertech.com>
2018-09-23 19:42:13 +0200

    macro args array, double quotes

--- lib/eva/lm/dmatrix.py
+++ lib/eva/lm/dmatrix.py
@@ -236,7 +236,7 @@ class DecisionRule(eva.item.Item):
         self.in_range_min_eq = False
         self.in_range_max_eq = False
         self.macro = None
-        self.macro_args = None
+        self.macro_args = []
         self.break_after_exec = False
         self.chillout_time = 0
         self.last_matched = 0
@@ -267,13 +267,7 @@ class DecisionRule(eva.item.Item):
         d['in_range_min_eq'] = self.in_range_min_eq
         d['in_range_max_eq'] = self.in_range_max_eq
         d['macro'] = self.macro
-        if self.macro_args:
-            d['macro_args'] = ''
-            for a in self.macro_args:
-                if d['macro_args']: d['macro_args'] += ' '
-                d['macro_args'] += '"{}"'.format(a)
-        else:
-            d['macro_args'] = None
+        d['macro_args'] = self.macro_args
         d['break_after_exec'] = self.break_after_exec
         d['chillout_time'] = self.chillout_time
         if not config:
@@ -352,11 +346,13 @@ class DecisionRule(eva.item.Item):
             self.macro = data['macro']
         if 'macro_args' in data:
             m = data['macro_args']
-            if m is not None:
+            if isinstance(m, str):
                 try:
                     m = shlex.split(m)
                 except:
                     m = m.split(' ')
+            elif not m:
+                m = []
             self.macro_args = m
         if 'break_after_exec' in data:
             self.break_after_exec = data['break_after_exec']
@@ -524,11 +520,10 @@ class DecisionRule(eva.item.Item):
                 except:
                     v = val.split(' ')
             else:
-                v = None
-            if self.macro_args != v:
-                self.macro_args = v
-                self.log_set(prop, val)
-                self.set_modified(save)
+                v = []
+            self.macro_args = v
+            self.log_set(prop, val)
+            self.set_modified(save)
             return True
         elif prop == 'break_after_exec':
             v = val_to_boolean(val)
