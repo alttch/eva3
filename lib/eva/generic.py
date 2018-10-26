8666c62437ae6894550a9c9549adc7a788095533
divisor <div@altertech.com>
2018-10-26 19:58:11 +0200

    generic

---
+++ lib/eva/generic.py
@@ -0,0 +1,67 @@
+ia_status_created = 0  # action just created
+ia_status_pending = 1  # put in global queue
+ia_status_queued = 2  # queued in item action processor queue
+ia_status_refused = 3  # refused to queue (item doesn't support queues)
+ia_status_dead = 4  # queueing into item queue took too long
+ia_status_canceled = 5  # canceled for some reason
+ia_status_ignored = 6  # queued but ignored to run (item already in state)
+ia_status_running = 7  # currently running
+ia_status_failed = 8  # failed to run
+ia_status_terminated = 9  # executed but terminated
+ia_status_completed = 10  # executed and finished successfully
+
+class GenericAction(object):
+
+    def __init__(self):
+        self._status = ia_status_created
+
+    def get_status(self):
+        return self._status
+
+    def set_status(self, status):
+        self._status = status
+
+    def is_processed(self):
+        return not self._status in [ia_status_created, ia_status_pending]
+
+    def is_finished(self):
+        return self._status in [
+            eva.item.ia_status_refused, eva.item.ia_status_dead,
+            eva.item.ia_status_canceled, eva.item.ia_status_ignored,
+            eva.item.ia_status_failed, eva.item.ia_status_terminated,
+            eva.item.ia_status_completed
+        ]
+
+    def is_status_created(self):
+        return self._status == ia_status_created
+
+    def is_status_pending(self):
+        return self._status == ia_status_pending
+
+    def is_status_queued(self):
+        return self._status == ia_status_queued
+
+    def is_status_refused(self):
+        return self._status == ia_status_refused
+
+    def is_status_dead(self):
+        return self._status == ia_status_dead
+
+    def is_status_canceled(self):
+        return self._status == ia_status_canceled
+
+    def is_status_ignored(self):
+        return self._status == ia_status_ignored
+
+    def is_status_running(self):
+        return self._status == ia_status_running
+
+    def is_status_failed(self):
+        return self._status == ia_status_failed
+
+    def is_status_terminated(self):
+        return self._status == ia_status_terminated
+
+    def is_status_completed(self):
+        return self._status == ia_status_completed
+
