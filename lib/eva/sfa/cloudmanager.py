84be539c3af1557a059fe27723e848c08f5c404f
divisor <div@altertech.com>
2018-11-10 00:40:20 +0100

    cloudmanager init

---
+++ lib/eva/sfa/cloudmanager.py
@@ -0,0 +1,42 @@
+import eva.core
+import eva.sfa.controller
+import eva.api
+
+from eva.api import cp_api_404
+
+import cherrypy
+import jinja2
+
+
+class CloudManager():
+
+    def __init__(self):
+        j2_loader = jinja2.FileSystemLoader(searchpath=eva.core.dir_lib +
+                                            '/cloudmanager')
+        self.j2 = jinja2.Environment(loader=j2_loader)
+
+    @cherrypy.expose
+    def index(self):
+        try:
+            template = self.j2.get_template('cm.j2')
+        except:
+            raise cp_api_404()
+        env = {'build': eva.core.product_build}
+        if eva.core.development:
+            env['development'] = True
+        return template.render(env)
+
+
+cp_cm_config = {}
+
+for u in ['css', 'fonts', 'i', 'js', 'lib']:
+    cp_cm_config['/' + u] = {
+        'tools.staticdir.dir': eva.core.dir_lib + '/cloudmanager/' + u,
+        'tools.staticdir.on': True
+    }
+
+
+def start():
+    if not eva.sfa.controller.cloud_manager: return
+    cherrypy.tree.mount(
+        CloudManager(), '/manage', config=cp_cm_config)
