2f17c7c66182f990fc4bbce02342fbece3ae093c
divisor <div@altertech.com>
2019-03-25 02:44:07 +0100

    nightly

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -41,7 +41,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2019032401
+product_build = 2019032501
 
 product_code = 'sfa'
 
--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@
 #fi
 
 VERSION=3.2.0
-BUILD=2019032401
+BUILD=2019032501
 
 [ "x${EVA_REPOSITORY_URL}" = "x" ] && EVA_REPOSITORY_URL=https://get.eva-ics.com
 
