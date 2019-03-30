feac05b102e34e3040ecc3281f36db36af80f67d
divisor <div@altertech.com>
2019-03-30 15:57:13 +0100

    fixes

--- ui/js/eva_sfa.js
+++ ui/js/eva_sfa.js
@@ -152,7 +152,8 @@ function eva_sfa_start() {
   eva_sfa_api_call('login', q, eva_sfa_after_login, function(code, msg, data) {
     eva_sfa_logged_in = false;
     eva_sfa_stop_engine();
-    if (eva_sfa_cb_login_error !== null) eva_sfa_cb_login_error(code, msg, data);
+    if (eva_sfa_cb_login_error !== null)
+      eva_sfa_cb_login_error(code, msg, data);
   });
 }
 
@@ -1249,10 +1250,13 @@ function eva_sfa_set_token_cookie() {
 }
 
 function eva_sfa_uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16)
-  );
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(
+    c
+  ) {
+    var r = (dt + Math.random() * 16) % 16 | 0;
+    dt = Math.floor(dt / 16);
+    return (c == 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  });
+  return uuid;
 }
