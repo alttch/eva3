5c2b57356f158cc61f9a6cbb53de748c348a5b47
divisor <div@altertech.com>
2019-03-31 03:29:16 +0200

    userhook example

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -42,7 +42,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2019033101
+product_build = 2019033102
 
 product_code = 'sfa'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019033101
+product_build = 2019033102
 
 product_code = 'uc'
 
