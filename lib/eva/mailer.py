b4156f4e392b5b697e4a18280a9b5feddaf15b36
divisor <div@altertech.com>
2019-04-03 03:28:52 +0200

    removed globals in mailer

--- lib/eva/mailer.py
+++ lib/eva/mailer.py
@@ -14,37 +14,38 @@ from eva.tools import parse_host_port
 
 from eva.exceptions import FunctionFailed
 
-_from = 'eva@' + platform.node()
+from types import SimpleNamespace
 
-_smtp_host = 'localhost'
-_smtp_port = 25
-_default_rcp = ['root']
+config = SimpleNamespace(
+    sender='eva@' + platform.node(),
+    smtp_host='localhost',
+    smtp_port=25,
+    default_rcp=['root'])
 
 default_port = 25
 
 
 def update_config(cfg):
-    global _smtp_host, _smtp_port, _from, _default_rcp
     try:
-        _smtp_host, _smtp_port = parse_host_port(
+        config.smtp_host, config.smtp_port = parse_host_port(
             cfg.get('mailer', 'smtp'), default_port)
     except:
-        _smtp_host = 'localhost'
-        _smtp_port = default_port
-    logging.debug('mailer.smtp = %s:%u' % (_smtp_host, _smtp_port))
+        config.smtp_host = 'localhost'
+        config.smtp_port = default_port
+    logging.debug('mailer.smtp = %s:%u' % (config.smtp_host, config.smtp_port))
     try:
-        _from = cfg.get('mailer', 'from')
+        config.sender = cfg.get('mailer', 'from')
     except:
-        _from = 'eva@' + platform.node()
-    logging.debug('mailer.from = %s' % _from)
+        config.sender = 'eva@' + platform.node()
+    logging.debug('mailer.from = %s' % config.sender)
     try:
-        _default_rcp = list(
+        config.default_rcp = list(
             filter(None, [
                 x.strip() for x in cfg.get('mailer', 'default_rcp').split(',')
             ]))
     except:
-        _default_rcp = ['root']
-    logging.debug('mailer.default_rcp = %s' % ', '.join(_default_rcp))
+        config.default_rcp = ['root']
+    logging.debug('mailer.default_rcp = %s' % ', '.join(config.default_rcp))
     return True
 
 
@@ -72,9 +73,11 @@ def send(subject=None, text=None, rcp=None):
     else: t = text
 
     if not rcp:
-        if not _default_rcp:
-            raise FunctionFailed('Neither recipient nor default recipient in config not specified')
-        else: _rcp = _default_rcp
+        if not config.default_rcp:
+            raise FunctionFailed('Neither recipient nor default ' +
+                                 'recipient in config not specified')
+        else:
+            _rcp = config.default_rcp
     else:
         if isinstance(_rcp, str): _rcp = [rcp]
         else: _rcp = rcp
@@ -83,13 +86,12 @@ def send(subject=None, text=None, rcp=None):
         msg = MIMEText(t)
 
         msg['Subject'] = s
-        msg['From'] = _from
-
+        msg['From'] = config.sender
 
         if len(_rcp) == 1:
             msg['To'] = _rcp[0]
-        sm = smtplib.SMTP(_smtp_host, _smtp_port)
-        sm.sendmail(_from, _rcp, msg.as_string())
+        sm = smtplib.SMTP(config.smtp_host, config.smtp_port)
+        sm.sendmail(config.sender, _rcp, msg.as_string())
         logging.debug('sending mail to %s, subject "%s"' % (', '.join(_rcp), s))
         sm.quit()
         return True
