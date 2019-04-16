e4cea8317913394123d36f5622e96960c80fae3d
divisor <div@altertech.com>
2019-04-16 13:14:24 +0200

    fixes

--- update.sh
+++ update.sh
@@ -147,9 +147,16 @@ ln -sf eva bin/eva-shell
 
 echo "- Updating tables"
 
-[ -f ./etc/uc.ini ] && ./sbin/eva-update-tables uc || exit 1
-[ -f ./etc/lm.ini ] && ./sbin/eva-update-tables lm || exit 1
-[ -f ./etc/sfa.ini ] && ./sbin/eva-update-tables sfa || exit 1
+if [ -f ./etc/uc.ini ]; then
+  ./sbin/eva-update-tables uc || exit 1
+fi
+
+if [ -f ./etc/lm.ini ]; then
+  ./sbin/eva-update-tables lm || exit 1
+fi
+if [ -f ./etc/sfa.ini ]; then
+  ./sbin/eva-update-tables sfa || exit 1
+fi
 
 echo "- Cleaning up"
 
