c5c91ad54f199a563d4396f64e6628570619cb34
divisor <div@altertech.com>
2019-05-16 16:33:40 +0200

    rc2

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -42,7 +42,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2019051602
+product_build = 2019051603
 
 product_code = 'lm'
 
--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@
 #fi
 
 VERSION=3.2.2
-BUILD=2019051602
+BUILD=2019051603
 
 [ "x${EVA_REPOSITORY_URL}" = "x" ] && EVA_REPOSITORY_URL=https://get.eva-ics.com
 
