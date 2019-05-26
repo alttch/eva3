d2e432ca82f7e306dcf88ed75d0888a9b881771c
divisor <div@altertech.com>
2019-05-27 00:33:17 +0200

    create_cookie

--- ui/js/eva_sfa.js
+++ ui/js/eva_sfa.js
@@ -1406,15 +1406,16 @@ function eva_sfa_read_cookie(name) {
   return null;
 }
 
-function eva_sfa_read_cookie(name) {
-  var nameEQ = name + '=';
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+function eva_sfa_create_cookie(name, value, days, path) {
+  var expires = '';
+  var p = '';
+  if (path !== undefined) p = path;
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toUTCString();
   }
-  return null;
+  document.cookie = name + '=' + value + expires + '; path=' + p;
 }
 
 function eva_sfa_erase_cookie(name, path) {
