426662327d0baaf28bb1d821274d5fa220b808c0
divisor <div@altertech.com>
2019-06-20 18:17:46 +0200

    test build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -42,7 +42,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2019062003
+product_build = 2019062004
 
 product_code = 'lm'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019062003
+product_build = 2019062004
 
 product_code = 'uc'
 
