939a422dc25939178c5aa7cfd044127dd9bb9785
divisor <div@altertech.com>
2019-06-20 19:14:46 +0200

    3.2.3 final, cm removed

--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019062008
+product_build = 2019062009
 
 product_code = 'uc'
 
--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@
 #fi
 
 VERSION=3.2.3
-BUILD=2019062008
+BUILD=2019062009
 
 [ "x${EVA_REPOSITORY_URL}" = "x" ] && EVA_REPOSITORY_URL=https://get.eva-ics.com
 
