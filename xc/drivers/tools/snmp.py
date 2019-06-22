498334e2b62dbdc1478b9416fd301ab2b04bcf49
divisor <div@altertech.com>
2019-06-22 02:22:20 +0200

    snmpwalk in snmp tool, driverapi ver = 6

--- xc/drivers/tools/snmp.py
+++ xc/drivers/tools/snmp.py
@@ -17,31 +17,60 @@ def get(oid,
         timeout=0,
         retries=0,
         rf=str,
-        snmp_ver=2):
+        snmp_ver=2,
+        walk=False):
+    """
+    Args:
+        oid: SNMP OID or MIB name
+        host: target host
+        port: target port (default: 161)
+        community: SNMP community (default: public)
+        timeout: max SNMP timeout
+        retries: max retry count (default: 0)
+        rf: return format: str, float, int or None
+        snmp_ver: SNMP version (default: 2)
+        walk: if True, SNMP walk will be performed
+
+    Returns:
+        If rf is set to None, raw pysnmp object is returned, otherwise parsed
+        to float, int or str
+
+        If walk is requested, list of pysnmp objects is returned
+    """
     result = []
+    snmpfunc = snmp_engine.nextCmd if walk else snmp_engine.getCmd
     try:
-        for (err_i, err_st, err_idx, vals) in snmp_engine.getCmd(
+        for (err_i, err_st, err_idx, vals) in snmpfunc(
                 snmp_engine.SnmpEngine(),
                 snmp_engine.CommunityData(community, mpModel=snmp_ver - 1),
                 snmp_engine.UdpTransportTarget(
                     (host, port), timeout=timeout, retries=retries),
                 snmp_engine.ContextData(),
-                snmp_engine.ObjectType(snmp_engine.ObjectIdentity(oid))):
+                snmp_engine.ObjectType(snmp_engine.ObjectIdentity(oid)),
+                lexicographicMode=False):
             if err_i or err_st:
                 logging.debug('snmp error: %s' % err_i)
-                return None
+                return
             else:
                 for v in vals:
-                    _v = str(v[1])
-                    try:
-                        if rf is float: _v = float(_v)
-                        if rf is int: _v = int(_v)
-                    except:
-                        _v = None
-                    return _v
+                    if walk:
+                        result.append(v)
+                    else:
+                        if rf is None: return v
+                        try:
+                            _v = str(v[1])
+                            try:
+                                if rf is float: _v = float(_v)
+                                if rf is int: _v = int(_v)
+                            except:
+                                _v = None
+                            return _v
+                        except:
+                            return
+        return result
     except:
         log_traceback()
-        return None
+        return
 
 
 def set(oid,
