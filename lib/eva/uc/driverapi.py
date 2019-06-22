a290e52869fb854b5fbf83ff9e6b4b584aa4f9c8
divisor <div@altertech.com>
2019-06-22 02:22:20 +0200

    snmpwalk in snmp tool, driverapi ver = 6

--- lib/eva/uc/driverapi.py
+++ lib/eva/uc/driverapi.py
@@ -2,7 +2,7 @@ __author__ = "Altertech Group, https://www.altertech.com/"
 __copyright__ = "Copyright (C) 2012-2019 Altertech Group"
 __license__ = "Apache License 2.0"
 __version__ = "3.2.3"
-__api__ = 5
+__api__ = 6
 
 import importlib
 import logging
