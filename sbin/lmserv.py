32acf23506de31232ff2daa8acf5c95882687b06
divisor <div@altertech.com>
2019-06-27 00:52:39 +0200

    bugfix build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -42,7 +42,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2019062601
+product_build = 2019062701
 
 product_code = 'lm'
 
--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -42,7 +42,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2019062601
+product_build = 2019062701
 
 product_code = 'sfa'
 
