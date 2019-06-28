16fc1c28fcc1e37f1b8bae5ab2b326be820ee9a4
divisor <div@altertech.com>
2019-06-28 23:15:24 +0200

    bugfix build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -42,7 +42,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2019062701
+product_build = 2019062801
 
 product_code = 'lm'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019062701
+product_build = 2019062801
 
 product_code = 'uc'
 
