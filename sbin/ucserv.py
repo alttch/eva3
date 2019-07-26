da9a620fdcf911895372ad628b3e0c8ef448933d
divisor <div@altertech.com>
2019-07-26 15:13:24 +0200

    test build

--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019072501
+product_build = 2019072601
 
 product_code = 'uc'
 
--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@
 #fi
 
 VERSION=3.2.4
-BUILD=2019072501
+BUILD=2019072601
 
 PYTHON3_MIN=4
 PYTHON_MINOR=`./python3/bin/python3 --version|cut -d. -f2`
