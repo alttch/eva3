cbdd8aef6cfc0411b55fb514df5361a543c87627
divisor <div@altertech.com>
2019-10-04 01:56:47 +0200

    bugfix build

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -42,7 +42,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2019100403
+product_build = 2019100404
 
 product_code = 'sfa'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -44,7 +44,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2019100403
+product_build = 2019100404
 
 product_code = 'uc'
 
