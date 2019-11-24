8125b501e30c17901232c2bbe0b7dc6365bd5bfb
Sergei S <div@altertech.com>
2019-11-24 01:35:01 +0000

    update.sh code fmt

--- update.sh
+++ update.sh
@@ -1,21 +1,16 @@
 #!/usr/bin/env bash
 
-#if [ "x`id -u`" != "x0" ] && [ "x$1" != "x--root" ]; then
-    #echo "Please run this script as root"
-    #exit 98
-#fi
-
 VERSION=3.2.6
 BUILD=2019111501
 
 PYTHON3_MIN=4
-PYTHON_MINOR=`./python3/bin/python3 --version|cut -d. -f2`
-if [ $PYTHON_MINOR -lt $PYTHON3_MIN ]; then
+PYTHON_MINOR=$(./python3/bin/python3 --version|cut -d. -f2)
+if [ "$PYTHON_MINOR" -lt "$PYTHON3_MIN" ]; then
   echo "Python 3.$PYTHON3_MIN is required"
   exit 1
 fi
 
-[ "x${EVA_REPOSITORY_URL}" = "x" ] && EVA_REPOSITORY_URL=https://get.eva-ics.com
+[ -z "${EVA_REPOSITORY_URL}" ] && EVA_REPOSITORY_URL=https://get.eva-ics.com
 
 OBS=""
 
@@ -31,21 +26,18 @@ fi
 
 source etc/eva_servers
 
-which jq > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if ! command -v jq > /dev/null; then
   echo "Please install jq"
   exit 1
 fi
 
 if [ -f ./sbin/eva-tinyapi ]; then
-    CURRENT_BUILD=`./sbin/eva-tinyapi -B`
-    
-    if [ $? != 0 ]; then
+    if ! CURRENT_BUILD=$(./sbin/eva-tinyapi -B); then
         echo "Can't obtain current build"
         exit 1
     fi
     
-    if [ $CURRENT_BUILD -ge $BUILD ]; then
+    if [ "$CURRENT_BUILD" -ge "$BUILD" ]; then
         echo "Your build is ${CURRENT_BUILD}, this script can update EVA ICS to ${BUILD} only"
         exit 1
     fi
@@ -57,10 +49,8 @@ echo "- Starting update to ${VERSION} build ${BUILD}"
 
 mkdir -p _update
 
-touch _update/test
-
-if [ $? -ne 0 ]; then
-    echo "Unable to write on partition. Read only file system?"
+if ! touch _update/test; then
+    echo "Unable to write. Read-only file system?"
     exit 1
 fi
 
@@ -94,35 +84,35 @@ echo "- Adding new runtime configs"
 
 for f in ${UC_NEW_CFG}; do
     [ ! -f $f ] && echo "{}" > $f
-    if [ "x$UC_USER" != "x" ]; then
-        chown ${UC_USER} $f
+    if [ "$UC_USER" ]; then
+        chown "${UC_USER}" "$f"
     fi
 done
 
 for f in ${UC_NEW_CFG_L}; do
     [ ! -f $f ] && echo "[]" > $f
-    if [ "x$UC_USER" != "x" ]; then
-        chown ${UC_USER} $f
+    if [ "$UC_USER" ]; then
+        chown "${UC_USER}" "$f"
     fi
 done
 
 for f in ${LM_NEW_CFG}; do
-    [ ! -f $f ] && echo "{}" > $f
-    if [ "x$LM_USER" != "x" ]; then
-        chown ${LM_USER} $f
+    [ ! -f "$f" ] && echo "{}" > $f
+    if [ "$LM_USER" ]; then
+        chown "${LM_USER}" "$f"
     fi
 done
 
 for f in ${LM_NEW_DIR}; do
-    mkdir -p $f
-    if [ "x$LM_USER" != "x" ]; then
-        chown ${LM_USER} $f
+    mkdir -p "$f"
+    if [ "$LM_USER" ]; then
+        chown "${LM_USER}" "$f"
     fi
 done
 
 if [ ! -d runtime/tpl ]; then
     mkdir runtime/tpl
-    chown ${UC_USER} runtime/tpl
+    chown "${UC_USER}" runtime/tpl
 fi
 
 if [ ! -d backup ]; then
@@ -141,10 +131,10 @@ mkdir -p ./xc/drivers/phi || exit 1
 mkdir -p ./xc/drivers/lpi || exit 1
 mkdir -p ./xc/extensions || exit 1
 
-(cd lib/eva/uc && ln -sf ../../../xc/drivers ) || exit 1
-(cd xc/drivers/phi && ln -sf ../../../lib/eva/uc/generic/generic_phi.py ) || exit 1
-(cd xc/drivers/lpi && ln -sf ../../../lib/eva/uc/generic/generic_lpi.py ) || exit 1
-(cd lib/eva/lm && ln -sf ../../../xc/extensions ) || exit 1
+(cd lib/eva/uc && ln -sf ../../../xc/drivers . ) || exit 1
+(cd xc/drivers/phi && ln -sf ../../../lib/eva/uc/generic/generic_phi.py . ) || exit 1
+(cd xc/drivers/lpi && ln -sf ../../../lib/eva/uc/generic/generic_lpi.py . ) || exit 1
+(cd lib/eva/lm && ln -sf ../../../xc/extensions . ) || exit 1
 (cd xc/extensions && ln -sf ../../lib/eva/lm/generic/generic_ext.py generic.py) || exit 1
 
 rm -f bin/eva-shell
@@ -169,9 +159,9 @@ echo "- Cleaning up"
 
 rm -rf _update
 
-CURRENT_BUILD=`./sbin/eva-tinyapi -B`
+CURRENT_BUILD=$(./sbin/eva-tinyapi -B)
 
-if [ $CURRENT_BUILD = $BUILD ]; then
+if [ "$CURRENT_BUILD" = "$BUILD" ]; then
     echo "- Current build: ${BUILD}"
     echo "---------------------------------------------"
     echo "Update completed. Starting everything back"
