b3fffe7981e171bfdd8ce946ef6176a5a69daa2e
Sergei S <div@altertech.com>
2020-08-06 04:26:31 +0200

    generate any pydoc

--- doc/generate-pydocs.py
+++ doc/generate-pydocs.py
@@ -2,7 +2,13 @@
 
 import glob
 import os
+import sys
 
-for i in glob.glob('pydoc/tpl_*.rst'):
-    out = i.replace('pydoc/tpl_', 'pydoc/pydoc_')
-    os.system(f'pydoc2rst {i} {out} /opt/eva/lib')
+if len(sys.argv) < 2:
+    for i in glob.glob('pydoc/tpl_*.rst'):
+        out = i.replace('pydoc/tpl_', 'pydoc/pydoc_')
+        os.system(f'pydoc2rst {i} {out} /opt/eva/lib')
+else:
+    for i in sys.argv[1:]:
+        os.system(
+            f'pydoc2rst pydoc/tpl_{i}.rst pydoc/pydoc_{i}.rst /opt/eva/lib')
