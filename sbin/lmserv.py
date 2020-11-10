166247dc6e359802b5e8ab454edc826dea35ee94
Sergei S <div@altertech.com>
2020-11-10 19:45:02 +0100

    test build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -44,7 +44,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2020111003
+product_build = 2020111004
 
 product_code = 'lm'
 
--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -45,7 +45,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2020111003
+product_build = 2020111004
 
 product_code = 'sfa'
 
