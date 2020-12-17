215c4644a2df820980f38c530c4fd42420fdc264
Sergei S <div@altertech.com>
2020-12-17 21:38:23 +0100

    test build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -44,7 +44,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2020121703
+product_build = 2020121704
 
 product_code = 'lm'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -47,7 +47,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2020121703
+product_build = 2020121704
 
 product_code = 'uc'
 
