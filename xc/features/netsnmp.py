6476b1958978691b23cc53e1f3043265addac027
Sergei S <div@altertech.com>
2020-12-19 01:37:33 +0100

    netsnmp

---
+++ xc/features/netsnmp.py
@@ -0,0 +1,19 @@
+from eva.features import OS_ID, OS_LIKE, UnsupportedOS
+from eva.features import append_python_libraries, remove_python_libraries
+from eva.features import install_system_packages
+
+python_libs = ['python3-netsnmp==1.1a1']
+
+
+def setup():
+    if OS_LIKE == 'debian':
+        install_system_packages(['libsnmp-dev'])
+    elif OS_LIKE == 'fedora':
+        install_system_packages(['net-snmp-devel'])
+    else:
+        raise UnsupportedOS
+    append_python_libraries(python_libs)
+
+
+def remove():
+    remove_python_libraries(python_libs)
