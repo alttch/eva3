55087a8fd9683829b584f123941609b8e6c614b9
Sergei S <div@altertech.com>
2020-12-19 06:11:50 +0100

    test build

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -45,7 +45,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2020121904
+product_build = 2020121905
 
 product_code = 'sfa'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -47,7 +47,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2020121904
+product_build = 2020121905
 
 product_code = 'uc'
 
