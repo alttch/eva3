bc9fa3f374e1321450a9d596d7fd43e36f5c520a
Sergei S <div@altertech.com>
2020-12-19 02:43:28 +0100

    defaultkey change feature

---
+++ xc/features/defaultkey.py
@@ -0,0 +1,16 @@
+from eva.features import InvalidParameter
+from eva.features import cli_call, restart_controller
+
+
+def setup(key=None):
+    if key is None or key == '':
+        raise InvalidParameter
+    data = cli_call('', 'server status', return_result=True)
+    changed = False
+    for c, v in data.items():
+        if v:
+            print(f'Changing default key for {c}')
+            cli_call(c, f'key set default key {key} -y', return_result=True)
+            changed = True
+    if changed:
+        restart_controller()
