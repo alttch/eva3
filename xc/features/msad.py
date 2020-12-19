2a4989929e8a369b8ecb457b12a1a6898e52d8bf
Sergei S <div@altertech.com>
2020-12-19 01:20:53 +0100

    fmt

--- xc/features/msad.py
+++ xc/features/msad.py
@@ -5,18 +5,19 @@ from eva.features import restart_controller
 from eva.features import InvalidParameter
 from eva.features import ConfigFile
 
+python_libs = ['easyad==1.0.9']
+
 
 def setup(host=None, domain=None, key_prefix='', ca=None):
     if not host or not domain:
         raise InvalidParameter
     if OS_LIKE == 'debian':
-        install_system_packages(
-            ['libsasl2-dev', 'libldap2-dev', 'libssl-dev'])
+        install_system_packages(['libsasl2-dev', 'libldap2-dev', 'libssl-dev'])
     elif OS_LIKE == 'fedora':
         install_system_packages(['openldap-devel', 'openssl-devel'])
     else:
         raise UnsupportedOS
-    append_python_libraries(['easyad==1.0.9'])
+    append_python_libraries(python_libs)
     config = {'host': host, 'domain': domain}
     if key_prefix:
         config['key_prefix'] = key_prefix
@@ -30,5 +31,5 @@ def setup(host=None, domain=None, key_prefix='', ca=None):
 def remove():
     with ConfigFile('sfa.ini') as fh:
         fh.remove_section('msad')
-    remove_python_libraries(['easyad==1.0.9'])
+    remove_python_libraries(python_libs)
     restart_controller('sfa')
