cf0bd2caa40c1cec39c0fd6657050655d2c2d40e
Sergei S <div@altertech.com>
2020-12-19 06:01:12 +0100

    venv

---
+++ xc/features/venv.py
@@ -0,0 +1,19 @@
+from eva.features import OS_ID, OS_LIKE, UnsupportedOS
+from eva.features import exec_shell, rebuild_python_venv, dir_eva
+from eva.features import install_system_packages
+
+from eva.tools import val_to_boolean
+
+
+def setup(from_scratch=False):
+    from_scratch = val_to_boolean(from_scratch)
+    if from_scratch:
+        from eva.features import print_warn
+        print_warn('Removing Python virtual environment')
+        print('Removing venv...')
+        # check to avoid rm at root
+        if dir_eva:
+            exec_shell(f'rm -rf {dir_eva}/python3')
+        else:
+            raise RuntimeError('EVA directory not detected')
+    rebuild_python_venv()
