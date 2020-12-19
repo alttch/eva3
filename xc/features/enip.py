df4806f49f7aa022435767a7db1e89f1e2bfeb76
Sergei S <div@altertech.com>
2020-12-19 01:20:40 +0100

    enip feature

---
+++ xc/features/enip.py
@@ -0,0 +1,27 @@
+from eva.features import append_python_libraries, remove_python_libraries
+
+from eva.features import download_phis, remove_phis
+
+from eva.tools import val_to_boolean
+
+python_libraries = ['cpppo==4.0.6']
+
+phis = [
+    'https://get.eva-ics.com/phi/enip/enip_sensor.py',
+    'https://get.eva-ics.com/phi/enip/enip_xvunit.py'
+]
+
+
+def setup(mods=False):
+    mods = val_to_boolean(mods)
+    append_python_libraries(python_libraries)
+
+    if mods:
+        download_phis(phis)
+
+
+def remove(mods=False):
+    remove_python_libraries(python_libraries)
+
+    if mods:
+        remove_phis(phis)
