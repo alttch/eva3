81e2facc043c30460ed84690de841329893481b0
Sergei S <div@altertech.com>
2020-12-20 12:32:50 +0100

    warn if no candidates

--- xc/features/node_masterkey.py
+++ xc/features/node_masterkey.py
@@ -15,6 +15,10 @@ def setup(node=None, key=None):
     if not node or not key:
         raise InvalidParameter
     controllers = get_controllers(node)
+    if not controllers:
+        from eva.features import print_warn
+        print_warn('no candidates found')
+        return
     for c in controllers:
         print(c)
         cli_call('sfa',
