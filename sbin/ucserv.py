f187c2893d72a2ce79bce3554361b5f20e58e988
Sergei S <div@altertech.com>
2021-02-04 22:13:16 +0100

    test build

--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -47,7 +47,7 @@ for production use uc-control only to start/stop UC
 """)
 
 
-product_build = 2021020201
+product_build = 2021020401
 
 product_code = 'uc'
 
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=3.3.2
-BUILD=2021020201
+BUILD=2021020401
 
 PYTHON3_MIN=6
 PYTHON_MINOR=$(./python3/bin/python3 --version|cut -d. -f2)
