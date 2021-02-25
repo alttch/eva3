c4539bc8ce2e48e1636c93e339496c073449e89a
Sergei S <div@altertech.com>
2021-02-25 11:45:36 +0100

    mailer feature

---
+++ xc/features/mailer.py
@@ -0,0 +1,41 @@
+from eva.features import restart_controller
+from eva.features import InvalidParameter
+from eva.features import ConfigFile
+
+from eva.tools import val_to_boolean
+
+import platform
+
+
+def setup(smtp=None,
+          default_from=None,
+          ssl=None,
+          tls=None,
+          login=None,
+          password=None):
+    if not smtp:
+        raise InvalidParameter
+    if not default_from:
+        default_from = f'eva@{platform.node()}'
+    config = {'smtp': smtp, 'from': default_from}
+    ssl = val_to_boolean(ssl)
+    tls = val_to_boolean(tls)
+    if ssl:
+        config['ssl'] = 'yes'
+    if tls:
+        config['tls'] = 'yes'
+    if login:
+        config['login'] = login
+    if password:
+        config['password'] = password
+    for c in ['uc', 'lm', 'sfa']:
+        with ConfigFile(f'{c}.ini') as fh:
+            fh.replace_section('mailer', config)
+            restart_controller(c)
+
+
+def remove():
+    for c in ['uc', 'lm', 'sfa']:
+        with ConfigFile(f'{c}.ini') as fh:
+            fh.remove_section('mailer')
+            restart_controller(c)
