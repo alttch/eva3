dcbd9d879d15a00d896128510acdca6bee5eaa14
Sergei S <div@altertech.com>
2021-03-05 01:44:26 +0100

    bugfix build

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -45,7 +45,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2021030102
+product_build = 2021030501
 
 product_code = 'sfa'
 
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=3.3.2
-BUILD=2021030102
+BUILD=2021030501
 
 PYTHON3_MIN=6
 PYTHON_MINOR=$(./python3/bin/python3 --version|cut -d. -f2)
