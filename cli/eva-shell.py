9e2dea313f8ab5cf3d42aeb05a3faa3971ee0f95
Sergei S <div@altertech.com>
2021-03-26 16:05:41 +0100

    remove lru cache from build/version

--- cli/eva-shell.py
+++ cli/eva-shell.py
@@ -1112,14 +1112,12 @@ sys.argv = {argv}
             return self.local_func_result_failed
 
     @staticmethod
-    @lru_cache(maxsize=None)
     def _get_build():
         with os.popen('{}/eva-tinyapi -B'.format(dir_sbin)) as p:
             data = p.read()
             return int(data.strip())
 
     @staticmethod
-    @lru_cache(maxsize=None)
     def _get_version():
         with os.popen('{}/eva-tinyapi -V'.format(dir_sbin)) as p:
             data = p.read()
