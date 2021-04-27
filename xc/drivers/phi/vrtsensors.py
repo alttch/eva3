ab18ff6c08dedc94a0001eb795356c1ba05b6c40
Sergei S <div@altertech.com>
2021-04-27 20:03:41 +0200

    random and incremental virtual values

--- xc/drivers/phi/vrtsensors.py
+++ xc/drivers/phi/vrtsensors.py
@@ -15,6 +15,11 @@ __config_help__ = [{
     'help': 'sensors value on load (default: None)',
     'type': 'float',
     'required': False
+}, {
+    'name': 'auto_modify',
+    'help': '"randomize" or "increment"',
+    'type': 'str',
+    'required': False
 }]
 __get_help__ = []
 __set_help__ = []
@@ -32,20 +37,26 @@ from eva.uc.driverapi import critical
 from eva.uc.driverapi import phi_constructor
 
 import eva.benchmark
+import threading
 
 
 class PHI(GenericPHI):
 
     @phi_constructor
     def __init__(self, **kwargs):
+        self.auto_modify = self.phi_cfg.get('auto_modify')
+        self.data_lock = threading.RLock()
         d = self.phi_cfg.get('default_value')
-        if d is None: d = None
+        if d is None:
+            d = None
         else:
             try:
                 d = float(d)
             except:
                 d = None
         self.data = {}
+        if self.auto_modify == 'increment' and d is None:
+            d = 0
         for i in range(1000, 1011):
             self.data[str(i)] = d
 
@@ -55,8 +66,31 @@ class PHI(GenericPHI):
                                        description='virtual sensor port #{}')
 
     def get(self, port=None, cfg=None, timeout=0):
-        if not port: return self.data
+        if not port:
+            if self.auto_modify:
+                if self.auto_modify == 'randomize':
+                    import random
+                    with self.data_lock:
+                        for d in self.data:
+                            self.data[d] = random.randint(1, 1000000000)
+                elif self.auto_modify == 'increment':
+                    with self.data_lock:
+                        for d in self.data:
+                            self.data[d] += 1
+                            if self.data[d] > 1000000000:
+                                self.data[d] = 0
+            return self.data
         try:
+            if self.auto_modify:
+                if self.auto_modify == 'randomize':
+                    import random
+                    self.data[str(port)] = random.randint(1, 1000000000)
+                elif self.auto_modify == 'increment':
+                    k = str(port)
+                    v = self.data[k] + 1
+                    if v > 1000000000:
+                        v = 0
+                    self.data[k] = v
             return self.data[str(port)]
         except:
             return None
@@ -83,12 +117,14 @@ class PHI(GenericPHI):
                                      'name': 'event_on_test_set',
                                      'type': 'bool'
                                  }])
+        if config.get('auto_modify') not in [None, 'randomize', 'increment']:
+            raise ValueError('Invalid auto_modify value')
 
     def test(self, cmd=None):
         if cmd == 'self':
             return 'OK'
         if cmd == 'get':
-            return self.data
+            return self.get()
         if cmd == 'critical':
             self.log_critical('test')
             return True
