25b56d9297d78869212da3df21e7c412700e7d03
Sergei S <div@altertech.com>
2021-06-12 00:53:21 +0200

    pid fix

--- sbin/safe-run.sh
+++ sbin/safe-run.sh
@@ -6,9 +6,11 @@ if [ -z "$1" ]; then
   echo "No params given"
   exit 10
 fi
-if ( kill -0 "$(cat "$1")" ) > /dev/null 2>&1; then
-  (>&2 echo "Process already active")
-  exit 11
+if [ -f "$1" ]; then
+  if kill -0 "$(cat "$1")" > /dev/null 2>&1; then
+    (>&2 echo "Process already active")
+    exit 11
+  fi
 fi
 echo $$ > "$1"
 shift
