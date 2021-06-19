30694f128dc4b9175115f438f74f7809de7bb1a6
Sergei S <div@altertech.com>
2021-06-19 23:49:49 +0200

    owfs build from source

--- xc/features/owfs.py
+++ xc/features/owfs.py
@@ -1,7 +1,9 @@
 from eva.features import OS_ID, OS_LIKE, UnsupportedOS
 from eva.features import append_python_libraries, remove_python_libraries
-from eva.features import install_system_packages
+from eva.features import install_system_packages, build_system_package
 from eva.features import val_to_boolean
+
+from pathlib import Path
 
 python_libs = ['onewire==0.2']
 
@@ -17,7 +19,23 @@ def setup(python_lib_only=None):
         elif OS_LIKE == 'fedora':
             install_system_packages(['owfs-libs', 'owfs-devel'])
         else:
-            raise UnsupportedOS
+            if not Path('/usr/include/owcapi.h').exists() and not Path(
+                    '/usr/local/include/owcapi.h').exists():
+                build_system_package(
+                    src=
+                    'https://github.com/owfs/owfs/releases/download/v3.2p4/owfs-3.2p4.tar.gz',
+                    sha256=
+                    'af0a5035f3f3df876ca15aea13486bfed6b3ef5409dee016db0be67755c35fcc',
+                    tdir='owfs-3.2p4',
+                    configure_args=[
+                        '--prefix=/usr', '--disable-owtcl', '--disable-owphp',
+                        '--disable-owpython', '--disable-zero',
+                        '--disable-owshell', '--disable-owhttpd',
+                        '--disable-owftpd', '--disable-owserver',
+                        '--disable-owperl', '--disable-owtap',
+                        '--disable-owmon', '--disable-owexternal'
+                    ],
+                    update_ld=True)
     append_python_libraries(python_libs)
 
 
