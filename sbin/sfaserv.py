d0193b56807f7784ca4a03bc1a08f645b41bedb4
Sergei S <div@altertech.com>
2021-06-23 01:05:30 +0200

    test build

--- sbin/sfaserv.py
+++ sbin/sfaserv.py
@@ -45,7 +45,7 @@ for production use sfa-control only to start/stop SFA
 """)
 
 
-product_build = 2021062303
+product_build = 2021062304
 
 product_code = 'sfa'
 
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=3.4.0
-BUILD=2021062303
+BUILD=2021062304
 
 PYTHON3_MIN=6
 if [ ! -d ./venv ]; then
