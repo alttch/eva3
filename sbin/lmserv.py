ccec834a66b614a3416482539bedeb0662f0e092
Sergei S <div@altertech.com>
2021-06-23 14:26:25 +0200

    test build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -44,7 +44,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2021062306
+product_build = 2021062307
 
 product_code = 'lm'
 
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=3.4.0
-BUILD=2021062306
+BUILD=2021062307
 
 PYTHON3_MIN=6
 if [ ! -d ./venv ]; then
