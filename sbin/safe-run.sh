d2e19626590a1c6ac47ada5282b5b2d1523fe5ff
Sergei S <div@altertech.com>
2021-11-09 19:29:16 +0100

    remove pid check in safe-run

--- sbin/safe-run.sh
+++ sbin/safe-run.sh
@@ -7,12 +7,6 @@ if [ -z "$1" ]; then
   echo "No params given"
   exit 10
 fi
-if [ -f "$1" ]; then
-  if kill -0 "$(cat "$1")" > /dev/null 2>&1; then
-    (>&2 echo "Process already active")
-    exit 11
-  fi
-fi
 echo $$ > "$1"
 shift
 while true; do
