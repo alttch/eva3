7233f89450cb107f7a3379f261f90660ef31cb15
Sergei S <div@altertech.com>
2021-11-11 00:53:49 +0100

    test build

--- sbin/lmserv.py
+++ sbin/lmserv.py
@@ -47,7 +47,7 @@ for production use lm-control only to start/stop LM PLC
 """)
 
 
-product_build = 2021110901
+product_build = 2021111101
 
 product_code = 'lm'
 
--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -49,7 +49,7 @@ for production use eva-control only to start/stop UC
 """)
 
 
-product_build = 2021110901
+product_build = 2021111101
 
 product_code = 'uc'
 
