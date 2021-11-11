31df02abbcde5db9a93412b4dbf9329eacbac401
Sergei S <div@altertech.com>
2021-11-12 00:14:08 +0100

    test build

--- sbin/ucserv.py
+++ sbin/ucserv.py
@@ -49,7 +49,7 @@ for production use eva-control only to start/stop UC
 """)
 
 
-product_build = 2021111102
+product_build = 2021111201
 
 product_code = 'uc'
 
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 VERSION=3.4.2
-BUILD=2021111102
+BUILD=2021111201
 
 PYTHON3_MIN=6
 if [ -d ./venv ]; then
